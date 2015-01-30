de8b4cb2c94cb75f963a259083b3c894a980d7d6
Administrator <Administrator@UBOSQUE.verano.net.co>
2015-01-30 17:23:01 -0500

    Calculadora

---
+++ src/Principal/CalculadoraAvanzada.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Principal;
+
+import Operaciones.Controlador;
+
+/**
+ *
+ * @author Administrator
+ */
+public class CalculadoraAvanzada {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Controlador c = new Controlador();
+        c.prioridad();
+    }
+    
+}
